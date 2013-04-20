4efa73ec413d7b56b413ac6272cea1227bf970b7
Dev Purkayastha <dev@forgreatjustice.net>
2013-04-20 16:21:53 -0400

    generate STL file

--- sculpture-webapp/sculpture.py
+++ sculpture-webapp/sculpture.py
@@ -1,12 +1,22 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import Response
+from flask import send_file
+from tempfile import NamedTemporaryFile
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET'])
+def home():
   return render_template('home.html')
 
+@app.route('/', methods=['POST'])
+def download_stl():
+  f = NamedTemporaryFile(delete=False)
+  f.write("STARMAP")
+  f.close()
+  return send_file(f.name, as_attachment=True, attachment_filename="starmap.stl", mimetype='application/octet-stream')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
