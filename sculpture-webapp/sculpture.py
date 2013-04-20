010a7986e8bdc01bec7251d8e352e2fd53304774
Dev Purkayastha <dev@forgreatjustice.net>
2013-04-20 15:33:40 -0400

    ok using template

--- sculpture-webapp/sculpture.py
+++ sculpture-webapp/sculpture.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+  return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
